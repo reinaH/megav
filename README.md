b1545912baafb220e2a23976f2a690cf916418e7
Reina <reh4fd@virginia.edu>
2022-09-30 19:37:57 -0300

    push mega

---
+++ README.md
@@ -0,0 +1,33 @@
+## how to run
+
+ -create python virtual env:
+ `python3 -m venv [name]`
+ -activate venv:
+`source [name]/bin/activate`
+- install deps
+`pip install -r requirements.txt`
+- run main script that solves whatever "current" phase youre in
+`python app.py`
+
+
+## my notes/comments:
+
+
+### note1: 
+challenge is designed to only return the current phase and does not allow you to move forward and ahead. we will not divide this out into different phase solutions. this affects design in that we just grab the current solution we need bc we assume all phases reasonably are part of the same problem and build off eachother. 
+
+### note2: 
+this solution is scalable. unless `limit_per_host=1` were noted the defualt is about 100 open connections at once. another solution to limiting and timing the resources is with a semaphone. in theory this could be scaled to hit millions of links in a reasonable span of time.
+
+i did not experiment too much on what an ideal limit is but while debugging i did find that if you place no limit the megaverse api does not like it and not all requests get processed appropriately despite logging that says that all calls were made and returned 200's. this issue was actually where i spent the most time on- i did not understand why the map was not displaying properly despite it processing all tasks and returning appropriate response codes, only after a while did i realize pacing the requests was the solution and not the async code. initially i thought that some part of the async code was causing the loop to leave tasks unfinished but the task counters reflected otherwise. 
+
+### note3:
+http bodies are case sensitive and if i tried to create an object w upper v lower case parameters (ex: BLUE vs blue) the map would render a question mark emoji at that index. i wanted to note that i noticed this but did not really have an opportunity to account for it as it was at the bottom of the todo list and then the map disapeared from view. 
+
+## if i could add improvements to app:
+
+i think what could make this assignment better is to add a 'currentmegaverse' endpoint. it would be nice to be able to just validate the current megaverse against the solution endpoint without having to physically look at it. that and risk the map 'disappearing'- if you do have the right solution as soon as you 'validate' you risk losing the current view. 
+
+
+
+
---
+++ app.py
@@ -0,0 +1,226 @@
+# imports. 
+import requests
+import time
+import aiohttp
+import asyncio
+import json
+from aiohttp import ClientSession
+
+# quick reference of space object options: 
+# SPACE
+# POLYANET
+# BLUE_SOLOON
+# WHITE_SOLOON
+# PURPLE_SOLOON
+# RED_SOLOON
+# LEFT_COMETH
+# RIGHT_COMETH
+# UP_COMETH
+# DOWN_COMETH
+
+# in a prod env this might be more similar to key and would be stored in an env doc and not so visibly
+candidateId = '4bc4ea3e-ec07-4b92-8112-5aed3e422f92'
+
+base = 'https://challenge.crossmint.io'
+headers = {
+  'Content-Type': 'application/json'
+}
+
+# class definitions
+
+class GoalMap():
+    # class attrs
+    goal_endpoint = '{}/api/map/{}/goal'.format(base, candidateId)
+    payload={}
+    headers={}
+
+    # instance attrs
+    def __init__(self):
+        self.goalmap = self.get_goal_map()
+        self.numrows = len(self.goalmap) #overkill but added so app doesnt break if we get a map that is not a nice NxN
+        self.numcols = len(self.goalmap[0])
+
+    # #####
+    def get_goal_map(self):
+        response = requests.request("GET", GoalMap.goal_endpoint, headers=GoalMap.headers, data=GoalMap.payload)
+
+        if response.status_code == 200:
+            return(response.json()['goal'])
+        else: 
+            return [[]] #when we get to the end this endpoint breaks so if not 200, just return empty map
+
+class AstralObjectsMapper():
+    # instance attrs
+    def __init__(self, goalmap):
+        self.astral_objects = self.astral_objects_mapper(goalmap, len(goalmap), len(goalmap[0]))
+    
+    # #####
+    def astral_objects_mapper(self, goalmap, numrows, numcols):
+        # this does one sweep and sorts and creates our objects to feed to our async client.
+        # 
+        # a very easy and 2-3 line alternative to do this would be to cast our solution array to a numpy array
+        # and have the numpy array return all indicies of each object ocurrance. chose do do this 
+        # because the numpy route would have in theory travered the matrix one time for every astral object
+        # whereas this is one sweep
+        #  
+        astralobjects = []
+        
+        for row in range(numrows):
+            for col in range(numcols):
+
+                curr = goalmap[row][col].split('_')
+                 
+                if curr[0] == 'SPACE':                    
+                    pass
+                elif curr[0] == 'POLYANET':                    
+                    currpolyanet = Polyanet((row,col))
+                    astralobjects.append(currpolyanet)
+
+                elif curr[1] == 'SOLOON':
+                    currsoloon = Soloon((row,col), curr[0].lower()) #http bodies are case sensitive
+                    astralobjects.append(currsoloon)
+
+                elif curr[1] == 'COMETH':
+                    currcometh = Cometh((row,col), curr[0].lower()) #http bodies are case sensitive
+                    astralobjects.append(currcometh)
+                            
+        return (astralobjects)
+
+
+# ################
+
+class  Polyanet():
+    # available HTTP methods:
+    # POST - arguments: row and col
+    # DELETE - arguments: row and col
+
+    # class attrs
+    endpoint = '/api/polyanets'
+
+    # instance attrs
+    def __init__(self, index):
+        self.name='POLYANET'
+        self.index=index
+        self.payload = self.get_payload(index[0], index[1])
+        self.url= (base + Polyanet.endpoint) #overkill as each instance has the same val but kept bc in theory is more scalable
+
+    # #####
+    def get_payload(self, row, column):
+        payload = json.dumps({
+            "candidateId": candidateId,
+            "row": row,
+            "column": column
+        })
+
+        return payload
+
+class  Soloon():
+    # available HTTP methods:
+    # POST - arguments: row, col, color
+    # DELETE - arguments: row, col
+
+    # class attrs
+    endpoint = '/api/soloons'
+
+    # instance attrs
+    def __init__(self, index, color):
+        self.name='SOLOON'
+        self.index=index
+        self.payload=self.get_payload(index[0], index[1], color)
+        self.color=color
+        self.url= (base + Soloon.endpoint) #overkill as each instance has the same val but kept bc in theory is more scalable
+
+    # #####
+    def get_payload(self, row, column, color):
+        payload = json.dumps({
+            "candidateId": candidateId,
+            "row": row,
+            "column": column,
+            "color": color, 
+        })
+        return payload
+    
+class Cometh():
+    # available HTTP methods:
+    # POST - arguments: row, col, direction
+    # DELETE - arguments: row, col
+
+    # class attrs
+    endpoint = '/api/comeths'
+
+    # instance attrs
+    def __init__(self, index, direction):
+        self.name='COMETH'
+        self.index=index
+        self.payload = self.get_payload(index[0], index[1], direction)
+        self.direction=direction
+        self.url= (base + Cometh.endpoint) #overkill as each instance has the same val but kept bc in theory is more scalable
+
+    # #####
+    def get_payload(self, row, column, direction):
+        payload = json.dumps({
+            "candidateId": candidateId,
+            "row": row,
+            "column": column,
+            "direction": direction, 
+        })
+        return payload
+
+class AstralObjectsAsyncClient():
+
+    def __init__(self, astrallist):
+        self.astrallist = astrallist
+        self.loop = asyncio.get_event_loop()
+        self.future = asyncio.ensure_future(self.run())
+
+    async def fetch(self,url, session, payload):
+        async with session.post(url, data=payload, headers=headers) as response:
+            # print('fetching', url, payload) #used to see what was going on. can delete. 
+            return await response.read()
+
+    async def run(self):
+        tasks = []
+        # Fetch all responses within one Client session,
+        # keep connection alive for all requests.
+
+        # the follwing lines are an alternative way to add rate/resource limits. 
+        # async with self.sem:
+            # await asyncio.sleep(0.1)
+
+
+        connector = aiohttp.TCPConnector(limit_per_host=1) #if left unlimited number is ~100 and crossmint api does not like that :(
+
+        async with ClientSession(connector=connector) as session:
+            for i in self.astrallist:
+                url = i.url
+                payload = i.payload
+
+                task = asyncio.ensure_future(self.fetch(url, session, payload))
+                tasks.append(task)
+
+            responses = await asyncio.gather(*tasks)
+            # you now have all response bodies in this variable
+            # print(responses, tasks) #used to see what was going on. can delete. 
+
+
+#  main 
+if __name__ == "__main__":
+
+
+    # get current goalmap. currently app is designed that if all phases are done it returns 500. 
+    #  so if 500 we return empty map so the universe doesnt explode. 
+    goalmap = GoalMap().goalmap
+
+
+    #goalmap to list of astral objects
+    astrallist = AstralObjectsMapper(goalmap).astral_objects
+
+    # start timer
+    start_time = time.time()
+
+    # run asyc client
+    asyncclient  = AstralObjectsAsyncClient(astrallist)
+    asyncclient.loop.run_until_complete(asyncclient.future)
+
+    # timer output
+    print("--- %s seconds ---" % (time.time() - start_time))
